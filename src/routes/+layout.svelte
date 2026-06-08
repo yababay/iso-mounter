67effc213109e13f23658043c2693162fd141bb5
Михаил Беляков <bigbelk@live.ru>
2026-06-08 15:59:05 +0300

    ok

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import '$lib/styles/root.scss';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
@@ -6,6 +7,11 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>ISO mounter</title>
 </svelte:head>
 
-{@render children()}
+<main>
+	<article>
+		{@render children()}
+	</article>
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import Page from './page.md'
+
+    onMount(() => {
+        const links = document.querySelectorAll('a')
+        for(const link of links) {
+            if(link.textContent !== 'mount') continue
+            const parent = link.parentNode
+            const icon = document.createElement('i')
+            icon.classList.add('bi')
+            icon.classList.add('bi-link')
+            link.textContent = ''
+            link.title = 'Подключить'
+            link.appendChild(icon)
+            link.addEventListener('click', async e => {
+                e.preventDefault()
+                if(!(success && danger)) return
+                const res = await fetch(link.href)
+                const { status } = res
+                const alert = status === 200 ? success : danger
+                alert.classList.remove('d-none')
+                await new Promise(yes => setTimeout(yes, 3000))
+                alert.classList.add('d-none')
+            })
+        }
+    })
+
+    let success: HTMLDivElement | undefined = $state()
+    let danger: HTMLDivElement | undefined = $state()
+</script>
+
+<Page />
+
+<div class="alert alert-success d-none" role="alert" bind:this={success}>
+  Образ успешно примонтирован.
+</div>
+
+<div class="alert alert-danger d-none" role="alert" bind:this={danger}>
+  При подключении ресурса произошла ошибка.
+</div>
+
+<style lang="scss">
+    .alert {
+        position: fixed;
+        top: 1rem;
+        right: 1rem;
+    }
+</style>
